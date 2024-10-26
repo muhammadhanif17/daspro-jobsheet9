5fb05cc8ececcfdde2bdb5589303f6be48cd2a75
Muhammad Hanif <hanifmuhammad1309@gmail.com>
2024-10-26 12:13:37 +0700

    p

--- Piramid.java
+++ Piramid.java
@@ -24,5 +24,6 @@ public class Piramid {
             spasi--;
             bintang += 2;
         }
+            
     }
 }
--- Tugas2.java
+++ Tugas2.java
@@ -3,27 +3,73 @@ import java.util.Scanner;
 public class Tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         //Deklarasi variabel
-        int jnisOlahraga = 0 , i = 1;
-        String nama;
-       
-        while (i <= 4) {
-            jnisOlahraga += 1;
-            if (jnisOlahraga == 1) {
-                System.out.println("Input nama atlet Badminton");
-            }else if (jnisOlahraga == 2) {
-                System.out.println("Input nama atlet Tenis Meja");
-            }else if (jnisOlahraga == 3) {
-                System.out.println("Input nama atlet Voli");
-            }else if (jnisOlahraga == 4) {
-                System.out.println("Input nama atlet Basket");
+        int i, j;
+        String nama, namaKampus, jenisOlahraga = "";
+        String daftarAtletBadminton = "";
+        String daftarAtletTenisMeja = "";
+        String daftarAtletVoli = "";
+        String daftarAtletBasket = ""; 
+
+        //Perulangan paling luar
+        do {
+            System.out.print("Masukkan nama Politeknik(Ketik 'batal' untuk berhenti): ");
+            namaKampus = sc.nextLine();
+
+            //Menghentikan program jika input 'batal'
+            if (namaKampus.equalsIgnoreCase("batal")) {
+                System.out.println("Program dihentikan");
+                break;
             }
-            for (int j = 1; j <= 5; j++) {
-                System.out.print("Masukkan nama atlet ke " + j + ": ");
-                nama = sc.nextLine();
+
+            //Perulangan kedua
+            for (i = 1; i <=4; i++) {
+                // Menyimpan nama atlet dalam string yang sesuai
+                switch (i) {
+                    case 1:
+                        jenisOlahraga = "Badminton";
+                        break;
+                    case 2:
+                        jenisOlahraga = "Tenis Meja";
+                        break;
+                    case 3:
+                        jenisOlahraga = "Voli";
+                        break;
+                    case 4:
+                        jenisOlahraga = "Basket";
+                        break;
+                }
+                System.out.println("Input nama atlet " + jenisOlahraga);
+
+                //Perulangan terdalam
+                for (j = 1; j <= 5; j++) {
+                    System.out.print("Masukkan nama atlet ke " + j + ": ");
+                    nama =sc.nextLine();
+                    // Menyimpan jenis olahraga dalam string yang sesuai
+                    switch (i) {
+                        case 1:
+                            daftarAtletBadminton += nama + ", ";
+                            break;
+                        case 2:
+                            daftarAtletTenisMeja += nama + ", ";
+                            break;
+                        case 3:
+                            daftarAtletVoli += nama + ", ";
+                            break;
+                        case 4:
+                            daftarAtletBasket += nama + ", ";
+                            break;
+                    }
+                }   
             }
-            System.out.println("");
-            i++;
-        }
+            //Output daftar nama atlet per kampus yang dikelompokkan berdasarkan jenis olahraga 
+            System.out.println("\nDaftar nama atlet " + namaKampus + ":");
+            System.out.println("Badminton: " + daftarAtletBadminton);
+            System.out.println("Tenis Meja: " + daftarAtletTenisMeja);
+            System.out.println("Voli: " + daftarAtletVoli);
+            System.out.println("Basket: " + daftarAtletBasket);
+            System.out.println();
+        } while (true);    
     }
 }
